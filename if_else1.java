572512b197428d8091d1fdabde0b09f90267ab15
SHIVRAJ DILIP GORE <107048041+meranaamshivraj@users.noreply.github.com>
2023-03-30 10:21:20 +0530

    Add files via upload

---
+++ if_else1.java
@@ -0,0 +1,16 @@
+package com.java.conditional_statement;
+
+public class if_else1 {
+	public static void main(String[] args) {
+		int marks = 60;
+		int criteria = 40;
+
+		if (marks >= criteria) {
+			System.out.println("Result pass");
+
+		} else {
+			System.out.println("Result Fail");
+		}
+
+	}
+}
